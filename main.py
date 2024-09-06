ac23b74703641b11a9ce9a6b269b1f802267469a
XAITYH <petrovskijpavel1@gmail.com>
2024-09-06 23:29:56 +0300

    starting a proj

---
+++ main.py
@@ -0,0 +1,10 @@
+from typing import Tuple
+import customtkinter as CTk
+
+class App(CTk.CTk):
+    def __init__(self):
+        super().__init__()
+        
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
