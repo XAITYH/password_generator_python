cd828d65f0b803fcf4ca97d3a3c6b42ff10a4d34
XAITYH <petrovskijpavel1@gmail.com>
2024-09-07 22:23:58 +0300

    What password will have like lwcase letters

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import tkinter
 import customtkinter as CTk
+
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 from PIL import Image
 
@@ -27,6 +30,73 @@ class App(CTk.CTk):
             command=self.set_password
         )
         self.btn_generate.grid(row=0, column=1)
+        
+        self.settings_frame = CTk.CTkFrame(master=self)
+        self.settings_frame.grid(row=2, column=0, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        
+        self.password_length_slider = CTk.CTkSlider(
+            master=self.settings_frame,
+            from_=0,
+            to=100,
+            number_of_steps=100,
+            command=self.slider_event
+        ) 
+        
+        self.password_length_slider.grid(
+            row=1,
+            column=0,
+            columnspan=3,
+            pady=(20, 20),
+            sticky="ew"
+        )
+        
+        self.password_length_entry = CTk.CTkEntry(master=self.settings_frame, width=50)
+        self.password_length_entry.grid(row=1, column=3, padx=(20, 10), sticky="ew")
+        
+        self.cb_digits_var = tkinter.StringVar()
+        self.cb_digits = CTk.CTkCheckBox(
+            master=self.settings_frame,
+            text="0-9",
+            variable=self.cb_digits_var,
+            onvalue=digits,
+            offvalue=""
+        )
+        
+        self.cb_digits.grid(row=2, column=0, padx=10)
+        
+        self.cb_lower_var = tkinter.StringVar()
+        self.cb_lower = CTk.CTkCheckBox(
+            master=self.settings_frame,
+            text="a-z",
+            variable=self.cb_lower_var,
+            onvalue=ascii_lowercase,
+            offvalue=""
+        )
+        
+        self.cb_lower.grid(row=2, column=1)
+        
+        self.cb_upper_var = tkinter.StringVar()
+        self.cb_upper = CTk.CTkCheckBox(
+            master=self.settings_frame,
+            text="A-Z",
+            variable=self.cb_upper_var,
+            onvalue=ascii_uppercase,
+            offvalue=""
+        )
+        self.cb_upper.grid(row=2, column=2)
+    
+        self.cb_symbol_var = tkinter.StringVar()
+        self.cb_symbol = CTk.CTkCheckBox(
+            master=self.settings_frame,
+            text="@#$%",
+            variable=self.cb_symbol_var,
+            onvalue=punctuation,
+            offvalue=""
+        )
+        self.cb_symbol.grid(row=2, column=3)
+        
+    def slider_event(self):
+        pass
         
     def set_password(self):
         pass
